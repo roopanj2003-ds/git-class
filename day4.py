64193d71923fdb3e3dc0b3f1499b4fe83d2bc916
roopanj2003-ds <roopanj2003@gmail.com>
2026-06-12 17:07:26 +0530

    initial commit

---
+++ day4.py
@@ -0,0 +1,92 @@
+
+# a= 2
+# b= 4
+
+# print ("ADD", a+b)
+# print ("sub", a-b)
+# print ("mul", a*b)
+# print ("division", a/b)
+# print ("modulo division", a%b)
+# print("floor division", a//b)
+# print("exponentary division", a**b)
+
+
+#comparison operator
+
+
+
+# a = 7
+# b = 10
+
+# print("equal to", a == b)
+# print("not equal to", a != b)
+# print("greater than", a > b)
+# print("less than", a < b)
+# print("greater than or equal to", a >=b)
+# print("less than or equal to", a <=b)
+
+#logical operator
+
+# and 
+# or
+# not
+
+# T AND T =True        T OR T =True
+# T AND F =False       T OR F =True
+# F AND T =FALSE       F OR T =True
+# F AND F =FALSE       F OR F =False
+
+# print("Logical and", (a < b) and (b > a))
+# print("Logical or", (a == b) or (a !=b))
+# print("Logical not", not(a==b))
+
+# Assignment Operater
+
+# a=5
+
+# a +=5
+# a-=2
+# a*=4
+# a//=2
+# a/=2
+# a%=3
+# a**=3
+# print ("result:",a)
+
+#conditional statement
+
+# if(condition):
+#     statements
+
+# city = input("Enter your tourist place: ")
+
+# if(city == "chennai"):
+
+#     print("Welcome to chennai")
+
+# attendanance = int(input("Enter your attendanace in percentage: "))
+
+# if(attendanance>=70):
+#     print("you are eligible to write a semester exam")
+    
+# else:
+#     print("you are not eligible to write a semester exam")
+
+
+age =int(input("enter your age: "))
+has_license = input("Do you have licence(y/n): ")
+
+# if(age>=18 and has_license == "y"):
+#     print("you can drive")
+# elif(age>18 and has_license == "n"):
+#     print("You can't drive")
+# else:
+#     print("You are under age. You cannot drive")
+
+
+if(has_license == "y"):
+    print("you can drive")
+elif(has_license == "n"):
+    print("You can't drive")
+else:
+    print("You are under age. You cannot drive")
