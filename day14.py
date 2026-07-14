6209aa71504a54e47cecaaa0b659594200b87ec3
roopanj2003-ds <roopanj2003@gmail.com>
2026-07-14 12:23:42 +0530

    initial commit

---
+++ day14.py
@@ -0,0 +1,32 @@
+ # def add(a, b, c , d, e, f, g, h):
+#     return a+b
+
+# print(add(10,20))
+
+
+# *args - positional arguments
+
+# def add1(a,b):
+#     print(a,b)
+
+# add1(20,10)
+# def add(*n):
+#     sum = 0
+#     for a in n:
+#     #    print(a)
+#        sum += a
+#     return sum
+# print(add(8,9,7,6,5,4,3,2,1,8,9,9))
+
+
+# **kwargs
+
+# def student_data(**k):
+#     print(k.get('age'))
+
+
+# student_data(
+#     name="Yogesh",
+#     age=18,
+#     city="Chennai"
+# )
